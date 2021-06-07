d2f9b6d375b0c26426817f7c713db54aa260b6bb
gdevine <gerarddevine@gmail.com>
2021-06-08 07:51:49 +1000

    Added a new subheading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>And it\'s changed! </h1>
+      <h1>And it's changed! </h1>
+      <h2>This is a new sub-title</h2>
     </div>
   );
 }
