66df5da1711d594006a1cabd7ca076bbbc018981
gdevine <gerarddevine@gmail.com>
2021-06-08 14:20:47 +1000

    Added a welcome message component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Welcome from "./Components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <h1>And it's changed! </h1>
-      <h2>This is a new sub-title</h2>
+      <h1>My Site</h1>
+      <Welcome />
     </div>
   );
 }
---
+++ src/Components/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Welcome() {
+  return (
+    <div>
+      <h2>Welcome to this website!</h2>
+      <h4>More coming soon....</h4>
+    </div>
+  );
+}
+
+export default Welcome;
